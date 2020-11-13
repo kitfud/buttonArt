0bd8bfa51acb10a2795afae1f8d8798f233dc77b
Kit Fuderich <kitfuderich@gmail.com>
2020-11-13 08:14:51 -0800

    button in place app class component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { auth, firestore, fireauth, firebasestore,storage } from './firebase/firebase';
+import firebase from 'firebase';
 
-function App() {
+
+class App extends React.Component {
+
+constructor(props){
+  super(props)
+  this.change = this.change.bind(this);
+  this.state = {
+    buttons:[],
+    buttonText:"Begin"
+  }
+}
+
+componentDidMount(){
+  firestore.collection('buttons').get().then(snapshot => {
+    let buttonArray = [];
+    snapshot.forEach(doc => {
+        const data = doc.data()
+        const _id = doc.id
+        buttonArray.push({_id, ...data });
+    })
+ 
+      this.setState(
+        {buttons: buttonArray}
+      )
+  
+})
+}
+
+change(){
+  this.setState(
+    {buttonText: "Now!"}
+  )
+}
+
+render(){
+const formatted = this.state.buttons.map((item)=>
+<div key={item._id}>{item.color}</div>
+)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<button onClick={this.change}>{this.state.buttonText}</button>
     </div>
-  );
+  )
+}
+
 }
 
 export default App;
